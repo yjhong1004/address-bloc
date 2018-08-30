27a6ff72693fd566525154a433d587b59c738549
Jenny Hong <yjhong1004@gmail.com>
2018-08-29 17:21:23 -0700

    final changes to this checkpoint

--- models/address_book.rb
+++ models/address_book.rb
@@ -6,20 +6,19 @@ class AddressBook
 
     def initialize
      @entries = []
-   end
-end
+    end
 
-def add_entry(name, phone_number, email)
-     # #9
-     index = 0
-     entries.each do |entry|
-     # #10
-       if name < entry.name
-         break
-       end
-       index+= 1
-     end
-     # #11
-     entries.insert(index, Entry.new(name, phone_number, email))
-   end
- end
+    def add_entry(name, phone_number, email)
+         # #9
+         index = 0
+         entries.each do |entry|
+         # #10
+           if name < entry.name
+             break
+           end
+           index+= 1
+         end
+         # #11
+         entries.insert(index, Entry.new(name, phone_number, email))
+    end
+end
